d9c9fd25e0043eca3b1d74f016341db4cf645fe9
SkylerRut <skaiste506@gmail.com>
2025-01-07 19:02:34 +0200

    Add files via upload

--- Graphics.c
+++ Graphics.c
@@ -79,7 +79,7 @@ void displayManual()
     printf("extra words by 0.05.\n");
     printf("And each incorrect guess resets the multiplier to 1.\n\n\n");
     printf("---\"Wrong answers\"---\n");
-    printf("If you enter three words in a row you will lose the game,\n");
+    printf("If you enter three wrong words in a row you will lose the game,\n");
     printf("so try your best to guess only the correct words!\n");
     printf("Good luck!\n");
 }
